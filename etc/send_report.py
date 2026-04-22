32aefa1935db3c73a9233ae68afba91fe72126d3
Sejung Oh <sejungoh86@gmail.com>
2026-04-22 16:56:14 +0900

    remove sensitive files, add gitignore

--- etc/send_report.py
+++
@@ -1,7 +0,0 @@
-from pathlib import Path
-import runpy
-
-
-if __name__ == "__main__":
-    script_path = Path(__file__).with_name("scripts") / "send_report.py"
-    runpy.run_path(str(script_path), run_name="__main__")
